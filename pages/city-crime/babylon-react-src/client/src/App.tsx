c4572493759fd2dd72f0860885e5aff7421d93c9
MD <info@md1usd.com>
2026-08-23 18:48:05 +0300

    Refactor App component to include routing and providers

--- pages/city-crime/babylon-react-src/client/src/App.tsx
+++ pages/city-crime/babylon-react-src/client/src/App.tsx
@@ -1,7 +1,35 @@
-import GameCanvas from "./components/GameCanvas";
+import { Toaster } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "@/pages/NotFound";
+import { Route, Switch, Router as WouterRouter } from "wouter";
+import { useHashLocation } from "wouter/use-hash-location";
+import ErrorBoundary from "./components/ErrorBoundary";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import Home from "./pages/Home";
+
+function Router() {
+  return (
+    <WouterRouter hook={useHashLocation}>
+      <Switch>
+        <Route path={"/"} component={Home} />
+        <Route path={"/404"} component={NotFound} />
+        <Route component={NotFound} />
+      </Switch>
+    </WouterRouter>
+  );
+}
 
 function App() {
-  return <GameCanvas />;
+  return (
+    <ErrorBoundary>
+      <ThemeProvider defaultTheme="dark">
+        <TooltipProvider>
+          <Toaster />
+          <Router />
+        </TooltipProvider>
+      </ThemeProvider>
+    </ErrorBoundary>
+  );
 }
 
 export default App;
