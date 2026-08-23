4d9264407a2ede78551d45f0aaaff22068815fae
MD <info@md1usd.com>
2026-08-24 00:47:29 +0300

    Add TitleScreen and game start logic to Home component

--- pages/city-crime/babylon-react-src/client/src/pages/Home.tsx
+++ pages/city-crime/babylon-react-src/client/src/pages/Home.tsx
@@ -1,10 +1,16 @@
 import { useState } from "react";
+import TitleScreen from "@/components/TitleScreen";
 import CharacterSelectScreen from "@/components/CharacterSelectScreen";
 import CityCrimeBabylon from "@/components/CityCrimeBabylon";
 import type { CharacterInfo } from "@/game/characters";
 
 export default function Home() {
+  const [started, setStarted] = useState(false);
   const [character, setCharacter] = useState<CharacterInfo | null>(null);
+
+  if (!started) {
+    return <TitleScreen onStart={() => setStarted(true)} />;
+  }
 
   if (!character) {
     return <CharacterSelectScreen onSelect={setCharacter} />;
