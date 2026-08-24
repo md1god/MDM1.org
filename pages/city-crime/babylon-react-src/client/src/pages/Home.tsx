a0a9575036bb93ad442f8b23f5fa186fd38ad73d
MD <info@md1usd.com>
2026-08-24 03:51:26 +0300

    Simplify Home component by removing selecting state
    Removed the selecting state and simplified the return logic.

--- pages/city-crime/babylon-react-src/client/src/pages/Home.tsx
+++ pages/city-crime/babylon-react-src/client/src/pages/Home.tsx
@@ -4,15 +4,10 @@ import CharacterSelectScreen from "@/components/CharacterSelectScreen";
 
 export default function Home() {
   const [started, setStarted] = useState(false);
-  const [selecting, setSelecting] = useState(false);
 
   if (!started) {
     return <TitleScreen onStart={() => setStarted(true)} />;
   }
 
-  if (!selecting) {
-    return <CharacterSelectScreen onSelect={() => setSelecting(true)} />;
-  }
-
-  return null;
+  return <CharacterSelectScreen onSelect={() => {}} />;
 }
